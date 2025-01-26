986bc45eacb83e77d48f15c7d4fa09c97c664e21
aimaneth <aiman.eth@proton.me>
2025-01-27 00:09:31 +0800

    update SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -76,6 +76,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/icon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
